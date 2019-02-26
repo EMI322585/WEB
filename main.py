691439db26e40c22ad9870eecfd2175a5f92ca63
EMI322585 <46558345+EMI322585@users.noreply.github.com>
2019-02-26 20:05:50 +0200

    update

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello")
+from flask import Flask, render_template
+app = Flask(__name__) #objekts Flask
+
+
+@app.route("/")
+def index():
+    render_template("index.html")
+
+@app.route("/about")
+def about():
+
+    return '<h1>About page</h><a href="/">Go back to main page</a>'# ārpusē lielās pēdiņas un iekšā mazās. Nedrīkst jaukt!
+
+
+if __name__ == '__main__': #pārbauda vai fails tiek izpildīts pa tiešo vai cita faila apakšdaļa
+    app.run() #palaiž objektu
